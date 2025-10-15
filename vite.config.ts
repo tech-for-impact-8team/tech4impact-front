16366eb5540aa9505ec4da863b3d968f7df23102
Tnalxmsk <alstnwkd990@naver.com>
2025-10-15 17:31:29 +0900

    :wrench: chore: add dependencies react-router, emotion, axios, tanstack-query

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr()],
+  assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.png', '**/*.jpeg', '**/*.gif'], // 이미지 파일 포함
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      {
+        find: '@app',
+        replacement: path.resolve(__dirname, 'src/app'),
+      },
+      {
+        find: '@pages',
+        replacement: path.resolve(__dirname, 'src/pages'),
+      },
+      {
+        find: '@dashboard',
+        replacement: path.resolve(__dirname, 'src/pages/dashboard'),
+      },
+      {
+        find: '@icon',
+        replacement: path.resolve(__dirname, 'src/shared/assets/icon'),
+      },
+      {
+        find: '@img',
+        replacement: path.resolve(__dirname, 'src/shared/assets/img'),
+      },
+      {
+        find: '@shared',
+        replacement: path.resolve(__dirname, 'src/shared'),
+      },
+    ],
+  },
+});
