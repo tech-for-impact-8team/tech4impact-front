32f4a2c948f16578339399cda05f893d837400f8
Tnalxmsk <alstnwkd990@naver.com>
2025-10-15 17:35:22 +0900

    :sparkles: feat: setup default project settings

---
+++ src/app/App.tsx
@@ -0,0 +1,17 @@
+import { AppRouterProvider, QueryProvider, ThemeProvider } from "@app/providers";
+import GlobalStyle from "@app/styles/GlobalStyle.tsx";
+
+
+function App() {
+
+  return (
+    <ThemeProvider>
+      <QueryProvider>
+        <GlobalStyle />
+        <AppRouterProvider />
+      </QueryProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/app/layouts/RootLayout.tsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+import styled from "@emotion/styled";
+
+const RootLayout = () => {
+  return (
+    <Wrapper>
+      <Outlet />
+    </Wrapper>
+  );
+};
+
+export default RootLayout;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100dvh;
+  background-color: white;
+  padding: env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left);
+`;
