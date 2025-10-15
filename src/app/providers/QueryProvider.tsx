e5d419f9b1c66b1549aa3a494217f4a5dfe77794
Tnalxmsk <alstnwkd990@naver.com>
2025-10-15 17:34:45 +0900

    :sparkles: feat: implements QueryProvider

---
+++ src/app/providers/QueryProvider.tsx
@@ -0,0 +1,18 @@
+import { type ReactNode, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const QueryProvider = ({ children }: { children: ReactNode }) => {
+  const [queryClient] = useState(() =>
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          staleTime: 1000 * 60 * 5,
+          refetchOnWindowFocus: false,
+        },
+      },
+    }));
+
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+};
+
+export default QueryProvider;
